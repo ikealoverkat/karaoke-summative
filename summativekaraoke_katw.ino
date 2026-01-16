87eb6d4f16ffa6c2f343768d48c72786eb1ab36e
kat <katwang7749@gmail.com>
2026-01-16 12:51:45 -0500

    add song choice user input via Serial Monitor

--- summativekaraoke_katw.ino
+++ summativekaraoke_katw.ino
@@ -8,14 +8,13 @@ Description: karaoke
 #include <LiquidCrystal_I2C.h>
 #include "pitches.h"
 
-int labubuSong[] = {}
-
-// int otherSong[] = {}
-
+int labubuSong[] = {};
 //2 = half note, 4 = quarter note, 8 = eigth note... etc
-int labubuNoteDurations[] = {} 
+int labubuNoteDurations[] = {};
+String labubuSongLyrics = "";
 
-
+int otherSong[] = {};
+int otherNoteDurations[] = {};
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 
@@ -25,13 +24,27 @@ void setup() {
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("hi"); //lcd debug
+  Serial.println("Pick a Song - type 1 for Bags (Clairo) and 2 for Labubu Song:");
 }
 
 void loop() {
-  playSong(labubuSong, labubuNoteDurations);
+  if (Serial.available() > 0)
+  {
+    int userInput = SerialreadStringUntil('\n');
+    userInput.trim();
+    Serial.println("" + userInput); //prints userInput for debugging
+    if (userInput !== 1 && userInput !== 2)
+    {
+      Serial.println("your input is invalid - try again!");
+      Serial.println("Pick a Song - type 1 for Bags (Clairo) and 2 for Labubu Song:");
+      return;
+    } //send an error message to the user if they didn't input 1 or 2 and then ask them for input again
+  }
+  
+  playSong(labubuSong, labubuNoteDurations, labubuSongLyrics);
 }
 
-void playSong(int song, int durations) {
+void playSong(int song, int durations, String lyrics) {
   bool isPlayingNote = false;
   int thisNote = 0;
   int noteDuration = 0;
